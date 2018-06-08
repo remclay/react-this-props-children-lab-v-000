8745e28e82be0dd38115638391f30d900bddf69d
remclay <remmy.lauren@gmail.com>
2018-06-08 12:30:20 +1000

    Done.

--- src/index.js
+++ src/index.js
@@ -1,17 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-// import Invitation from './components/Invitation';
-// import ThemedParty from './components/ThemedDecorations';
-//
-// ReactDOM.render(
-//   <div>
-//     <Invitation>Hey, you should totally come to this awesome party I'm throwing!</Invitation>
-//     <ThemedParty theme="heaven">
-//       <p>A decoration</p>
-//       <p>Some other decoration</p>
-//       <p>A last decoration</p>
-//     </ThemedParty>
-//   </div>,
-//   document.getElementById('global')
-// );
+import Invitation from './components/Invitation';
+import ThemedParty from './components/ThemedDecorations';
+
+ReactDOM.render(
+  <div>
+    <Invitation> Hey, you should totally come to this awesome party I'm throwing!</Invitation>
+    <ThemedParty theme="heaven">
+      <p>A decoration</p>
+      <p>Some other decoration</p>
+      <p>A last decoration</p>
+    </ThemedParty>
+  </div>,
+  document.getElementById('global')
+);
