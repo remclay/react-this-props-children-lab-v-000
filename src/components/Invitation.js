3157d427b0c221b78f2ba026e3625b80c71419b6
remclay <remmy.lauren@gmail.com>
2018-06-08 11:50:34 +1000

    Done.

--- src/components/Invitation.js
+++ src/components/Invitation.js
@@ -1,1 +1,13 @@
 // Code Invitation Component Here
+// import React from 'react';
+//
+// export default class Invitation extends React.Component {
+//   render() {
+//     return (
+//       <div>
+//         <h1>You&#39;ve been invited!</h1>
+//         <div>{this.props.children}</div>
+//       </div>
+//     )
+//   }
+// }
--- src/components/ThemedDecorations.js
+++ src/components/ThemedDecorations.js
@@ -1,1 +1,14 @@
 // Code ThemedDecoration Component Here
+// import React from 'react';
+// import Invitation from './components/Invitation';
+//
+// export default class ThemedDecorations extends React.Component {
+//   render() {
+//     return (
+//       // let childrenWithClassName = React.children.map(this.props.children, child => {
+//       //   return ( <div className-"special-class-name">{child}</div> );
+//       // });
+//       // return childrenWithClassName
+//     )
+//   }
+// }
